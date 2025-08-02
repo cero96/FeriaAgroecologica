04e6bb5bcad37eda2e2fabf9b4f426ff5c4b074c
Pedro <tamayoandres211294@gmail.com>
2025-08-01 19:02:16 -0500

    Productorxs

--- comercio-electronico/frontend/src/pages/Productores.jsx
+++ comercio-electronico/frontend/src/pages/Productores.jsx
@@ -5,17 +5,17 @@ import Particule from '../components/Particule'; // importa tu componente Partic
 const productores = [
   {
     nombre: 'Leonardo Quilo',
-    descripcion: 'Productora de lacteos.',
+    descripcion: 'Productor que elabora lácteos frescos y naturales, con métodos sostenibles y de calidad.',
     imagen: '/Images/Productores/2.png'
   },
   {
     nombre: 'Lucia Espinosa',
-    descripcion: 'Agricultor especializado en frutas.',
+    descripcion: 'Productora dedicada al cultivo de hortalizas frescas y orgánicas, con prácticas sostenibles y de calidad.',
     imagen: '/Images/Productores/3.png'
   },
   {
     nombre: 'Maria Elvira Perez',
-    descripcion: 'Cultiva desde los 5 años',
+    descripcion: 'Productora dedicada al cultivo de frutas frescas, naturales y libres de químicos, comprometida con la calidad y la sostenibilidad.',
     imagen: '/Images/Productores/1.png'
   },
 ];
